bb38a3aa2e5fd2cab9aa55f2d64f65129e9faf4b
DevRamona <r.ingabire@alustudent.com>
2025-07-17 16:08:00 +0200

    chore: convert main server file to JavaScript and integrate category routes

---
+++ src/controllers/categoryController.js
@@ -0,0 +1,50 @@
+const Category = require('../models/Category');
+
+exports.createCategory = async (req, res, next) => {
+  try {
+    const category = new Category(req.body);
+    await category.save();
+    res.status(201).json(category);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getCategoryById = async (req, res, next) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) return res.status(404).json({ message: 'Category not found' });
+    res.json(category);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateCategory = async (req, res, next) => {
+  try {
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!category) return res.status(404).json({ message: 'Category not found' });
+    res.json(category);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteCategory = async (req, res, next) => {
+  try {
+    const category = await Category.findByIdAndDelete(req.params.id);
+    if (!category) return res.status(404).json({ message: 'Category not found' });
+    res.json({ message: 'Category deleted' });
+  } catch (err) {
+    next(err);
+  }
+}; 
